a4474594ad5e835590dd6c5ef102493d65a75858
Govinda <gov70659063@gmail.com>
2026-02-04 09:31:37 +0530

    server.js file updated

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.post("/chat", async (req, res) => {
 
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + process.env.GEMINI_API_KEY,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + process.env.AIzaSyCn6Wf0G51tTmWcgJSSFrsdXGaL890zXiY,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
